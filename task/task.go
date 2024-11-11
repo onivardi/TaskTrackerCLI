d6b567b6a2a18f508b42992b82c89b29b472fa9c
Valdeir Rodrigues <vardii.rodrigues@gmail.com>
2024-11-11 11:00:22 -0400

    add Task struct

---
+++ task/task.go
@@ -0,0 +1,11 @@
+package task
+
+import "time"
+
+type Task struct {
+	Id          int
+	Description string
+	Status      string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
---
+++ task/task_test.go
@@ -0,0 +1,17 @@
+package task_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/onivardi/TaskTrackerCLI/task"
+)
+
+func TestTask(t *testing.T) {
+	t.Parallel()
+	_ = task.Task{
+		Id:          1,
+		Description: "My first task",
+		CreatedAt:   time.Now(),
+	}
+}
